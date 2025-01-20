247f65fbd540520d132d095ef65a5668222f0312
Gia Nagpal <gianagpal08@gmail.com>
2025-01-19 23:19:21 -0500

    Initial commit with lab1.py

---
+++ week1/lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# First and Last Name: Gia Nagpal
+
+# Defining Variables
+array = [1, 4, 7, 9]
+
+# Order of Operations
+a, b, c, d = 1, 2, 3, 4
+e = (a - (b ** c) // d) + (a % c)
+print("Answer",e)
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: ")) + 22
+print("Now showing the shop items filtered by age:", userAge)
